e36a3521fbe199477b2b67437a0beffa09b40c3b
samruddhitiwari <121943400+samruddhitiwari@users.noreply.github.com>
2024-05-16 19:14:52 +0530

    Update NovaNectar_internship_task-1.py

--- NovaNectar_internship_task-1.py
+++ NovaNectar_internship_task-1.py
@@ -1,23 +1,48 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Util.Padding import pad, unpad
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+from os import urandom
+import base64
 
 def encrypt_text_AES(text, password):
-    salt = get_random_bytes(16)
-    key = PBKDF2(password, salt, 16, count=1000000)
-    cipher = AES.new(key, AES.MODE_CBC)
-    ciphertext = cipher.encrypt(pad(text.encode(), AES.block_size))
-    return salt + cipher.iv + ciphertext
+    salt = urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=1000000,
+        backend=default_backend()
+    )
+    key = kdf.derive(password.encode())
+    iv = urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(algorithms.AES.block_size).padder()
+    padded_data = padder.update(text.encode()) + padder.finalize()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    return base64.b64encode(salt + iv + ciphertext).decode('utf-8')
 
 def decrypt_text_AES(encrypted_text, password):
-    salt = encrypted_text[:16]
-    iv = encrypted_text[16:32]
-    ciphertext = encrypted_text[32:]
-    key = PBKDF2(password, salt, 16, count=1000000)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    decrypted_text = unpad(cipher.decrypt(ciphertext), AES.block_size)
-    return decrypted_text.decode()
+    encrypted_data = base64.b64decode(encrypted_text)
+    salt = encrypted_data[:16]
+    iv = encrypted_data[16:32]
+    ciphertext = encrypted_data[32:]
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=1000000,
+        backend=default_backend()
+    )
+    key = kdf.derive(password.encode())
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    padded_data = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+    decrypted_data = unpadder.update(padded_data) + unpadder.finalize()
+    return decrypted_data.decode('utf-8')
 
 # Example usage
 password = "mysecretpassword"
